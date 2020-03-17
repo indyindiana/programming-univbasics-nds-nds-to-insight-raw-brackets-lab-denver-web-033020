cf4b8429bc168752ce9afa771323517b1a03cb77
indiana danaher <indiana.danaher@gmail.com>
2020-03-17 21:22:49 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -13,10 +13,13 @@ def directors_totals(nds)
       gross += directors_database[director_count][:movies][movie_count][:worldwide_gross]
       movie_count += 1 
     end 
-    return gross
+    director_gross.push(directors_database[director_count] => gross)
+    
 
     director_count += 1 
   end
+  return director_gross
+  
   
   # Remember, it's always OK to pretty print what you get *in* to make sure
   # that you know what you're starting with!
