bb43b7fcfb2d45e9f99be04d5217031befc20559
indiana danaher <indiana.danaher@gmail.com>
2020-03-17 21:20:18 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -8,7 +8,7 @@ def directors_totals(nds)
   director_count = 0 
   while director_count < directors_database.length do
     movie_count = 0 
-    
+    gross = 0 
     while movie_count < directors_database[director_count][:movies].length do
       gross += directors_database[director_count][:movies][movie_count][:worldwide_gross]
       movie_count += 1 
